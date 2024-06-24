e51e3c49ca93a9b13c1bb9d6824a76f7ea004dfa
Justin Siek <justinsiek05@gmail.com>
2024-06-23 21:22:34 -0700

    implemented postgresql to backend

--- server/server.py
+++ server/server.py
@@ -1,8 +1,15 @@
 from flask import Flask, jsonify, request
 from flask_cors import CORS
+import psycopg2
 
 app = Flask(__name__)
 CORS(app)
+
+DB_NAME = 'postgres'
+DB_USER = 'write_user'
+DB_PASSWORD = 'password'
+DB_HOST = 'localhost'
+DB_PORT = '5432'
 
 @app.route('/api/double', methods=['GET'])
 def double_number():
@@ -10,6 +17,15 @@ def double_number():
 
     number = int(number_str)
     doubled_number = number * 2
+
+    conn = psycopg2.connect(dbname=DB_NAME, user=DB_USER, password=DB_PASSWORD, host=DB_HOST, port=DB_PORT)
+    cursor = conn.cursor()
+    cursor.execute(f'INSERT INTO project_starter.projectstarter (original, doubled) values ({number}, {doubled_number})')
+
+    conn.commit()
+    cursor.close()
+    conn.close()
+
 
     return jsonify({'result': doubled_number})
 
